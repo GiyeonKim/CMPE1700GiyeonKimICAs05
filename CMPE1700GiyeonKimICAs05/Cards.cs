332779f6b1b422401fe54d613ae6961db4bdd205
Pinkmong <gkim9@VDI-CNT2015-095.nait.ca>
2015-10-30 10:15:02 -0600

    ICAs05
    Deck of cards

---
+++ CMPE1700GiyeonKimICAs05/Cards.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE1700GiyeonKimICAs05
+{
+    public enum Suit
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades
+    }
+    public enum Faces
+    {
+        Two =1,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+    public struct Cards
+    {
+        
+        public Suit Suits;
+        public Faces Face;
+
+
+        public override string ToString()
+        {
+            return Face + " of " + Suits;
+        }
+    }
+}
---
+++ CMPE1700GiyeonKimICAs05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE1700GiyeonKimICAs05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an array of structs
+
+            
+            Cards[] Deck = new Cards[Enum.GetValues(typeof(Faces)).Length+43];
+
+         
+            int i = 0;
+            foreach (Suit s in Enum.GetValues(typeof(Suit)))
+            {
+                Deck[i].Suits = s;
+                Deck[i++].Suits = s;
+                
+
+                foreach (Faces f in Enum.GetValues(typeof(Faces)))
+                {
+                    Deck[i].Face = f;
+                    Deck[i++].Face = f;
+                    Console.WriteLine(f + " of " + s);
+                }
+            }
+            Console.ReadKey();
+           
+        }
+
+        
+        
+    }
+}
